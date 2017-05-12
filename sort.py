a4cc1c63260d89e8760c772786c5ad5ac13aa2c2
wilderbe4st <alantomjose@gmail.com>
2017-05-12 10:54:37 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -2,16 +2,18 @@
 #Program to sort contents of a fle into corresponding directories
 # to run this use : python2 sort.py
 
+
 import os
 import shutil
 
 
 
 path=raw_input("Give path >")
+
 if not os.path.exists(path):
     print "%s does not exist \n Exiting...."%path
     exit(0)
-print "%s exists\n...........\n" %path
+print "%s exists\n.................\n" %path
 
 
 
@@ -22,7 +24,7 @@ images=["jpg","png","gif","tif"]
 video=["avi","mp4","mov","wmv","flv","mkv"]
 music=["mp3","wav"]
 execu=["exe","bat","sh","py","php","rpm","deb"]
-othr=["doc","pdf","rar","gz","tar"]
+othr=["doc","pdf","rar","gz","tar","torrent"]
 ####<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>####
 
 path = path.replace("/","//")
@@ -38,27 +40,33 @@ fil=[]
     
 for folder,subf,filenm in os.walk(path):
     fil.extend(filenm)
-       
+    
 for fname in fil:
     ext=fname.split(".")
+    
    
     if ext[1] in images:
         shutil.move((path+'//'+fname),(path+dirs["img"]+'//'+fname))
         
     elif ext[1] in texts:
-        shutil.move((path+'//'+fname),(path+dirs["txt"]+'//'+fname))
-
+       shutil.move((path+'//'+fname),(path+dirs["txt"]+'//'+fname))
+       
     elif ext[1] in video:
-        shutil.move((path+'//'+fname),(path+dirs["vid"]+'//'+fname))
-
+       shutil.move((path+'//'+fname),(path+dirs["vid"]+'//'+fname))
+        
     elif ext[1] in execu:
-        shutil.move((path+'//'+fname),(path+dirs["exec"]+'//'+fname))
+       shutil.move((path+'//'+fname),(path+dirs["exec"]+'//'+fname))
 
     elif ext[1] in music:
-        shutil.move((path+'//'+fname),(path+dirs["msc"]+'//'+fname))
+       shutil.move((path+'//'+fname),(path+dirs["msc"]+'//'+fname))
 
     elif ext[1] in othr:
-        shutil.move((path+'//'+fname),(path+dirs["othr"]+'//'+fname))
+       shutil.move((path+'//'+fname),(path+dirs["othr"]+'//'+fname))
         
 
-        
+       
+for key,val in dirs.items():
+    try:
+        os.rmdir(path+val)
+    except:
+        continue
