d36ae394c311cf815361d19075a051fef72d1cfc
wilderbe4st <alantomjose@gmail.com>
2017-05-12 22:25:19 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,77 @@
+
+#Program to sort contents of a fle into corresponding directories
+
+import os
+import shutil
+
+
+
+path=raw_input("Give path >")
+#path="/home/alan/Desktop"
+#path="/home/alan/Downloads"
+if not os.path.exists(path):
+    print "%s does not exist \n Exiting...."%path
+    exit(0)
+print "%s exists\n.................\n" %path
+
+
+
+####<<<<<<<<<<<<<<<< sortables >>>>>>>>>>>>>>>>>####
+dirs={ "txt":"//Text","vid":"//Video","img":"//Images","msc":"//Music","exec":"//Executables","othr":"//Others"}
+texts=["txt"]
+images=["jpg","png","gif","tif"]
+video=["avi","mp4","mov","wmv","flv","mkv"]
+music=["mp3","wav"]
+execu=["exe","bat","sh","py","php","rpm","deb"]
+othr=["doc","pdf","rar","gz","tar","ttf","html"] 
+####<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>####
+
+path = path.replace("/","//")
+print path
+for key,val in dirs.items():
+    if not os.path.exists(path+val):
+        os.mkdir(path+val)
+        print "creating directory %s"%val
+    else:
+        print "%s exits"%val
+        continue
+fil=[]
+
+    
+for folder,subf,filenm in os.walk(path):
+    fil.extend(filenm)
+   
+    
+
+   
+for fname in fil:
+    ext=fname.split(".")
+    try:
+         
+       if ext[1] in images:
+           shutil.move((path+'//'+fname),(path+dirs["img"]+'//'+fname))
+        
+       elif ext[1] in texts:
+           shutil.move((path+'//'+fname),(path+dirs["txt"]+'//'+fname))
+       
+       elif ext[1] in video:
+           shutil.move((path+'//'+fname),(path+dirs["vid"]+'//'+fname))
+        
+       elif ext[1] in execu:
+           shutil.move((path+'//'+fname),(path+dirs["exec"]+'//'+fname))
+
+       elif ext[1] in music:
+           shutil.move((path+'//'+fname),(path+dirs["msc"]+'//'+fname))
+
+       elif ext[1] in othr:
+           shutil.move((path+'//'+fname),(path+dirs["othr"]+'//'+fname))
+    except: continue
+       
+for key,val in dirs.items():
+    try:
+        os.rmdir(path+val)
+    except:
+        continue
+
+print "*"*20
+print "\n\n\n\t all your files in %s are sorted"%path
